90c644d7b107507c0399f8e4e878735c6fa8d9f9
Gary Seip <seipg@merrimack.edu>
2022-10-24 19:00:04 -0400

    Updated comments. Declared a function for animating the deck shuffling. Created constants for card dimensions.

--- Card.js
+++ Card.js
@@ -12,8 +12,13 @@
         constructBox(textures);
     }
 
-    construcBox(textures)
+    constructBox(textures)
     {
+        // 655 and 930 are respectively the width and height of the card images.
+        const width = 655 / 200;
+        const height = 930 / 200;
+        // This will make a full deck of cards look like a square when viewed along the length of the cards.
+        const depth = width / 52;
         var currTexture = 0; 
         var materials = 
         [
@@ -22,10 +27,10 @@
             new THREE.MeshBasicMaterial({map: textures[currTexture++]}),
             new THREE.MeshBasicMaterial({map: textures[currTexture++]}),
             new THREE.MeshBasicMaterial({map: textures[currTexture++]}),
-            new THREE.MeshBasicMaterial({map: textures[currTexture++]})
+            new THREE.MeshBasicMaterial({map: textures[currTexture]})
         ];
 
-        var geometry = new THREE.BoxGeometry()
+        var geometry = new THREE.BoxGeometry(width, height, depth);
     }
  }
 
--- Deck.js
+++ Deck.js
@@ -11,6 +11,10 @@
         this.score;
     }
 
+    /**
+     * This function shuffles the order of the cards in the array.
+     * Uses code taken and modified from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array.
+     */
     shuffle() 
     {
         let currentIndex = this.cards.length,  randomIndex;
@@ -25,7 +29,17 @@
           // And swap it with the current element.
           [this.cards[currentIndex], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[currentIndex]];
         }
-      }
+    }
+
+    /**
+     * This function plays an animation to visually indicate the shuffling of the cards.
+     * 
+     * @param {Number} delta The difference in time between the current frame and the previosu frame.
+     */
+    shuffleAnimation(delta)
+    {
+        // Not yet implemented.
+    }
  }
 
  export {Deck};
