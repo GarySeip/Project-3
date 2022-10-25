e66f9b0f1b4da140e492e0f63bf423d2bfe24b38
Gary Seip <seipg@merrimack.edu>
2022-10-25 16:14:07 -0400

    Updated keyboard input method. Implemented a function for determining the highest valued card in play.

--- War.js
+++ War.js
@@ -18,7 +18,54 @@
         this.players = players;
     }
 
+    /**
+     * This function handles progressing the game forward after each press of the "n" key.
+     */
+    advanceGameStep()
+    {
+        // Gets the top car of each player's deck.
+        // There needs to be different code for updating the cards during a war.
+        for(var i = 0; i < this.players.length; i++)
+            this.playerCards[i] = this.players[i].deck.pop();
 
+        // To-do: Code to move cards into place.
+         
+
+        findHighest();
+
+
+    }
+
+    /**
+     * This function handles finding the highest value card in play or 
+     * @returns -1 if there is a tie for the highest, 0 if player 1 has the highest, 1 if player 2 has the highest,
+     *           2 if player 3 has the highest.
+     */
+    findHighest()
+    {
+        var val0 = this.playerCards[0].value;
+        var val1 = this.playerCards[1].value;
+        var val2 = this.playerCards[2].value;
+
+        // Checks for ties.
+        if
+        (
+            val0 == val1 && val0 >= val2 ||
+            val0 == val2 && val0 >= val1 ||
+            val1 == val2 && val1 >= val0
+        )
+            return -1;
+        // Checks for player 1 having the highest valued card.
+        else if(val0 > val1 && val0 > val2)
+            return 0;
+
+        // Checks for player 2 having the highest valued card.
+        else if(val1 > val0 && val1 > val2)
+            return 1;
+        // Checks for player 3 having the highest valued card.
+        else
+            return 2;
+    }
  }
 
  export {War};
